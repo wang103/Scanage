0af7ee6ab28a682eb6955e2a9d62d1dec3807bbf
Tianyi Wang <wang103@illinois.edu>
2016-01-10 22:13:38 -0800

    Finished config camera

--- Scanage/ScanningViewController.swift
+++ Scanage/ScanningViewController.swift
@@ -6,14 +6,51 @@
 //  Copyright © 2016 Tianyi. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
-class ScanningViewController: UIViewController {
+class ScanningViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    var captureSession: AVCaptureSession?   // coordinate the flow of data from input device to output device
+    var captureVideoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeView: UIView?
+    
+    
+    func configureVideoCapture() -> Bool {
+        
+        let captureDevice: AVCaptureDevice! = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        let captureDeviceInput: AVCaptureDeviceInput
+        do {
+            captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        }
+        catch {
+            // Show error message
+            let alert = UIAlertController(title: "Device Error", message: "Device not supported for this app.",
+                preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            return false
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(captureDeviceInput as AVCaptureInput)
+        
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        configureVideoCapture()
     }
 
     override func didReceiveMemoryWarning() {
