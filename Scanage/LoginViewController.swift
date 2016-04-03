75577b16c4463cade99e7963b5e891dc96dfa1e9
Tianyi Wang <wang103@illinois.edu>
2016-04-03 13:09:51 -0700

    Added spinner to login view

--- Scanage/LoginViewController.swift
+++ Scanage/LoginViewController.swift
@@ -11,13 +11,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
     @IBOutlet var usernameField: UITextField!
     @IBOutlet var passwordField: UITextField!
+    @IBOutlet var loginButton: UIButton!
     
     @IBOutlet var errorMsgLabel: UILabel!
     
     
     @IBAction func login(sender: UIButton) {
         print("Login button pressed")
+    }
+    
+    
+    func startSpinner() {
+        errorMsgLabel.text = ""
+        usernameField.enabled = false
+        passwordField.enabled = false
+        loginButton.enabled = false
+        
+        spinner.startAnimating()
+    }
+    
+    func stopSpinner() {
+        self.spinner.stopAnimating()
+        
+        usernameField.enabled = true
+        passwordField.enabled = true
+        loginButton.enabled = true
+    }
+    
+    func initSpinner() {
+        self.view.bringSubviewToFront(spinner)
+    }
+    
+    override func viewDidLoad() {
+        initSpinner()
     }
 }
