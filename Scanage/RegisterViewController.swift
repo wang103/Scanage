57c978db168311daf0b9d41c8bce90f9b494b0b0
Tianyi Wang <wang103@illinois.edu>
2016-04-08 01:38:23 -0700

    Init setup for register view

---
+++ Scanage/RegisterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterViewController.swift
+//  Scanage
+//
+//  Created by Tianyi Wang on 4/8/16.
+//  Copyright © 2016 Tianyi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+}
