3a630222341025e2e3f5e651b53c1039232b4378
Tianyi Wang <wang103@illinois.edu>
2016-04-08 23:13:45 -0700

    Add action

--- Scanage/RegisterViewController.swift
+++ Scanage/RegisterViewController.swift
@@ -30,4 +30,48 @@ class RegisterViewController: UIViewController {
         self.view.removeFromSuperview()
         self.removeFromParentViewController()
     }
+    
+    @IBAction func register(sender: UIButton) {
+        let username = self.usernameField.text
+        if username == nil || username!.isEmpty {
+            errorMsgLabel.text = "Username cannot be empty"
+            return
+        }
+        
+        let password1 = self.password1Field.text
+        if password1 == nil || password1!.isEmpty {
+            errorMsgLabel.text = "Password cannot be empty"
+            return
+        }
+        
+        let password2 = self.password2Field.text
+        if password1! != password2! {
+            errorMsgLabel.text = "Passwords do not match"
+            return
+        }
+        
+        let email = self.emailField.text
+        if email == nil || email!.isEmpty {
+            errorMsgLabel.text = "Email cannot be empty"
+            return
+        }
+        
+        var firstName = self.firstnameField.text
+        if firstName == nil {
+            firstName = ""
+        }
+        firstName = firstName!.stringByTrimmingCharactersInSet(
+            NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        )
+        
+        var lastName = self.lastnameField.text
+        if lastName == nil {
+            lastName = ""
+        }
+        lastName = lastName!.stringByTrimmingCharactersInSet(
+            NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        )
+        
+        
+    }
 }
