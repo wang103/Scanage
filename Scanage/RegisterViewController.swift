2796c1cf93927fc1cf683f0b699fff4ad7fdd5cf
Tianyi Wang <wang103@illinois.edu>
2016-04-08 21:47:07 -0700

    Connect UI to code for register view

--- Scanage/RegisterViewController.swift
+++ Scanage/RegisterViewController.swift
@@ -11,6 +11,20 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var password1Field: UITextField!
+    @IBOutlet var password2Field: UITextField!
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var firstnameField: UITextField!
+    @IBOutlet var lastnameField: UITextField!
+    
+    @IBOutlet var errorMsgLabel: UILabel!
+    
+    @IBOutlet var submitButton: UIButton!
+    
+    
     @IBAction func removeFromParent(sender: UIBarButtonItem) {
         self.willMoveToParentViewController(nil)
         self.view.removeFromSuperview()
